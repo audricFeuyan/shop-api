2c940336d93a4e1e8748d5857ac0d8e409c11565
FEUYAN TCHOUO AUDRIC CAUDIN <audric.feuyan@mboadigital.tech>
2024-12-25 11:50:49 +0100

    Validate fields

--- shop/serializers.py
+++ shop/serializers.py
@@ -1,4 +1,4 @@
-from rest_framework.serializers import ModelSerializer, SerializerMethodField
+from rest_framework.serializers import ModelSerializer, SerializerMethodField, ValidationError
 
 from shop.models import Category, Product, Article
 
@@ -6,14 +6,26 @@ from shop.models import Category, Product, Article
 class CategoryListSerializer(ModelSerializer):
     class Meta:
         model = Category
-        fields = ['id', 'date_created', 'date_updated', 'name']
+        fields = ['id', 'date_created', 'date_updated', 'name', 'description']
+
+    def validate_name(self, value):
+        if Category.objects.filter(name=value).exists():
+            raise ValidationError('Category already exists')
+        
+        return value
+    
+    def validate(self, data):
+        if data['name'] not in data['description']:
+            raise ValidationError('Name must be in description')
+        
+        return data
 
 class CategoryDetailSerializer(ModelSerializer):
 
     products = SerializerMethodField()
     class Meta:
         model = Category
-        fields = ['id', 'date_created', 'date_updated', 'name', 'products']
+        fields = ['id', 'date_created', 'date_updated', 'name', 'description', 'products']
 
     def get_products(self, instance):
         queryset = instance.products.filter(active=True)
@@ -33,3 +45,15 @@ class ArticleSerializer(ModelSerializer):
     class Meta:
         model = Article
         fields = ['id', 'date_created', 'date_updated', 'name', 'price', 'product']
+
+    def validate_price(self, value):
+        if value < 1:
+            raise ValidationError('Price must be greater than 1 euro')
+        
+        return value
+    
+    def validate_product(self, value):
+        if value.active is False:
+            raise ValidationError('Related product must be active')
+        
+        return value
--- shop/views.py
+++ shop/views.py
@@ -1,25 +1,48 @@
 from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.decorators import action
 
 from shop.models import Category, Product, Article
 from shop.serializers import CategoryDetailSerializer, CategoryListSerializer, ProductSerializer, ArticleSerializer
 
-class CategoryViewset(ReadOnlyModelViewSet):
+
+class MultipleSerializerMixin:
+    detail_serializer_class = None
+
+    def get_serializer_class(self):
+        if self.action == 'retrieve' and self.detail_serializer_class is not None:
+            return self.detail_serializer_class
+        
+        return super().get_serializer_class()
+    
+class CategoryViewset(MultipleSerializerMixin, ReadOnlyModelViewSet):
     serializer_class = CategoryListSerializer
     detail_serializer_class = CategoryDetailSerializer
+
+    @action(detail=True, methods=['post'])
+    def disable(self, request, pk):
+        self.get_object().disable()
+        return Response()
 
     def get_queryset(self):
         return Category.objects.filter(active=True)
     
-    def get_serializer_class(self):
-        if self.action == 'retrieve':
-            return self.detail_serializer_class
-        return super().get_serializer_class()
+class AdminCategoryViewset(MultipleSerializerMixin, ModelViewSet):
+    serializer_class = CategoryListSerializer
+    detail_serializer_class = CategoryDetailSerializer
+
+    def get_queryset(self):
+        return Category.objects.all()
 
     
 class ProductViewset(ModelViewSet):
     serializer_class = ProductSerializer
+
+    @action(detail=True, methods=['post'])
+    def disable(self, request, pk):
+        self.get_object().disable()
+        return Response()
 
     def get_queryset(self):
         query_set = Product.objects.filter(active=True)
@@ -42,3 +65,17 @@ class ArticleViewset(ModelViewSet):
             query_set = query_set.filter(product_id=product_id)
 
         return query_set
+    
+class AdminArticleViewset(ModelViewSet):
+    serializer_class = ArticleSerializer
+
+    def get_queryset(self):
+        query_set = Article.objects.all()
+        product_id = self.request.GET.get('product_id')
+
+        if product_id is not None:
+            query_set = query_set.filter(product_id=product_id)
+
+        return query_set
+    
+        
